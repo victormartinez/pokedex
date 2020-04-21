d6070f4ae9c36a8fbff2fe160db77df1ec71c850
Victor Martinez <vcrmartinez@gmail.com>
2020-04-21 14:20:34 -0300

    chore: remove console.log

--- src/actions/pokedexActions.js
+++ src/actions/pokedexActions.js
@@ -2,7 +2,6 @@ import dispatcher from '../appDispatcher';
 import actionTypes from './actionTypes';
 
 export function capturePokemon(pokemon) {
-  console.log("CAPTURE POKEMON ACTION");
   dispatcher.dispatch({
     actionType: actionTypes.CAPTURE_POKEMON,
     pokemon: pokemon
